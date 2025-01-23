586ecb5e321af0ea5e7d7f9681e03d8cf4fc3b8d
Shakaet <abdshakaet@gmail.com>
2025-01-23 10:44:35 +0600

    add

--- app/component/NavBar.jsx
+++ app/component/NavBar.jsx
@@ -37,12 +37,12 @@ const NavBar = async() => {
 // </div>
 //         </div>
 
-<div className="navbar bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg">
+ <div className="navbar bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg">
   <div className="flex-1">
     <a className="text-2xl font-bold px-4 py-2 tracking-wide">NextJs Blog Viewer</a>
   </div>
   <div className="flex-none">
-    <ul className="menu menu-horizontal space-x-4 px-4">
+    <ul className="menu md:menu-horizontal space-x-4 px-4">
       <li>
         <Link
           href="/"
@@ -80,7 +80,14 @@ const NavBar = async() => {
       )}
     </ul>
   </div>
-</div>
+</div> 
+
+
+
+
+
+
+
 
     );
 };
--- app/profile/page.js
+++ app/profile/page.js
@@ -60,6 +60,8 @@ const ProfilePage = async () => {
     // Retrieve user session details
     const { getUser } = getKindeServerSession();
     const user = await getUser();
+    
+
 
     if (!user) {
       // Redirect to login if no user is found
@@ -67,14 +69,18 @@ const ProfilePage = async () => {
     }
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gradient-to-br from-purple-100 to-pink-500">
-        <div className="p-8 bg-white shadow-lg rounded-lg text-center">
-          <h2 className="text-4xl font-bold text-gray-800 mb-4">
-            Welcome to your profile!
-          </h2>
-         
+        <div>
+          {
+          user && <div className="flex items-center justify-center h-screen bg-gradient-to-br from-purple-100 to-pink-500">
+          <div className="p-8 bg-white shadow-lg rounded-lg text-center">
+            <h2 className="text-4xl font-bold text-gray-800 mb-4">
+              Welcome to your profile!
+            </h2>
+           
+          </div>
         </div>
-      </div>
+        }
+        </div>
       
     );
   } 
